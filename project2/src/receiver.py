af447a1e68d22517da34003739480d156503dc90
None <None>
2021-03-30 17:06:19 -0700

    change to receiver

--- project2/src/receiver.py
+++ project2/src/receiver.py
@@ -6,27 +6,27 @@ from socket import *
 import sys
 import select
 
-host="10.0.0.1"
+host="0.0.0.0"
 port = int(sys.argv[1])
 s = socket(AF_INET,SOCK_DGRAM)
 s.bind((host,port))
 
 addr = (host,port)
-buf=1024
-result = ""
-
+buf = 1024
 #f = open("RECEIVED_FILE",'wb')
 
 data,addr = s.recvfrom(buf)
+result = ""
 try:
 	while(data):
 		result = data.decode('utf-8')
 		#print(type(result))
-		print(result)
+		print(result.strip("\n"))
 		#f.write(data)
 		s.settimeout(2)
 		data,addr = s.recvfrom(buf)
 except timeout:
 	#f.close()
 	s.close()
+	sys.exit
 	print("File Downloaded")
