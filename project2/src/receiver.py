8c55b24c16e4280645bef4ed1e8331ca01b11bf2
None <None>
2021-04-07 20:57:48 -0700

    Project2 final push

--- project2/src/receiver.py
+++ project2/src/receiver.py
@@ -5,7 +5,11 @@
 from socket import *
 import sys
 import select
-import json 
+import json
+
+def parse_list(list):
+	for value in list:
+		print(value)
 
 host="0.0.0.0"
 port = int(sys.argv[1])
@@ -16,14 +20,46 @@ addr = (host,port)
 buf = 1024
 
 data,addr = s.recvfrom(buf)
-result = ""
+resultlist = []
+count = -1
+endflag = False
+
 try:
 	while(data):
 		result = data.decode('utf-8')
-		result = json.loads(result)
+		if(result[4:7] == "END"):
+			packetdata = result[7:-1]
+			packetsegment = result[0:4]
+			endflag = True
+		else:
+			packetdata = result[4:-1]
+#			print(packetdata)
+			packetsegment = result[0:4]
+		resultlist.append(packetdata)
+		count = count+1
+		ack = packetsegment + "added"
+		s.sendto(ack.encode(), addr)
+		if(len(resultlist) >=  2):
+			if(resultlist[count] == resultlist[count-1]):
+				resultlist.pop()
+				count = count - 1
+		if(len(resultlist) == 100):
+			parse_list(resultlist)
+			resultlist = []
+			count = -1
+#		result = json.loads(result)
 #		print(result.strip("\n"))
-		print((result["data"]).strip("\n"))
-		s.settimeout(2)
+#		print((result["data"]).trip("\n"))
+		s.settimeout(5)
+		if(endflag == True):
+			break
 		data,addr = s.recvfrom(buf)
+		s.settimeout(None)
 except timeout:
 	s.close()
+	parse_list(resultlist)
+
+
+
+s.close()
+parse_list(resultlist)
--- project2/src/sender.py
+++ project2/src/sender.py
@@ -2,43 +2,92 @@
 
 #!/usr/bin/env python
 from socket import *
-import socket 
+import socket
 import sys
-import json 
-import os 
+import os
+import collections
+
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 host = sys.argv[1]
 port = int(sys.argv[2])
 buf =1024
 addr = (host,port)
+s.setblocking(0)
 
+packetdata = ""
+packetsequence = "1203"
+ackbuffer = []
+count = 0
+endflag = False
+windowsize = 10
+windowcount = 0
+slidingwindow = collections.deque()
 #command = "ping -qc3 " + host + " 2>&1 | awk -F\'/\' \'END{print(/^rtt/? \"\"$6\"\":\"1\")}\'"
 #temp = os.popen(command).read()
 
 #TimeOut_RTT = float(temp)
 
-datagram  = {"sequence_number": 23134,"data":""}
-datagram_buffer = []
-
+line = sys.stdin.readline()
 while True:
-	line = sys.stdin.readline()
 	if not line:
-		packet = json.dumps(datagram)
-		if(s.sendto(bytes(packet.encode('utf-8')),addr)):
+		datagram = packetsequence + "END" + packetdata
+#		datagram = packetsequence + line
+		datagram = datagram.encode()
+#		slidingwindow.append(line.strip())
+		if(s.sendto(bytes(datagram),addr)):
 			print("sending...")
-			#print(datagram)
+#			print(datagram)
+			windowcount = windowcount + 1
+		try:
+			s.settimeout(2)
+			ACK, addrs = s.recvfrom(buf)
+			ackdata = ACK.decode('utf-8')
+			ackbuffer.append(ackdata)
+#			print(ackbuffer)
+			count = count + 1
+		except socket.timeout:
+			print("packet loss...retransmitting")
 		break
-	if((sys.getsizeof(datagram["data"])-1 + sys.getsizeof(line)) < buf):
+	if((sys.getsizeof(packetdata)-1 + sys.getsizeof(line)) < buf):
 #		data += line
-		datagram["data"] += str(line)
+		packetdata += str(line)
+#		print(packetdata)
+		line = sys.stdin.readline()
 	else:
-		datagram = json.dumps(datagram)
-		if(s.sendto(bytes(datagram.encode('utf-8')),addr)):
+#		print(line)
+#		print(packetdata)
+		datagram = packetsequence + packetdata
+		datagram = datagram.encode()
+		if(s.sendto(bytes(datagram),addr)):
 			print("sending...")
-			#print(datagram)
+#			print(packetdata)
+#		packetdata = ""
+#		line = sys.stdin.readline()
+#		packetdata = str(line)
+#		print(packetdata)
+		try:
+			s.settimeout(2)
+			ACK, addrs = s.recvfrom(buf)
+			s.settimeout(None)
+			ackdata = ACK.decode('utf-8')
+			ackbuffer.append(ackdata)
+#			print(ackbuffer)
+			packetdata = ""
+			if not line:
+				break
+#			print(count)
+			count = count + 1
+		except socket.timeout:
+			print("packet loss...retransmitting")
+
+
+#		if(s.sendto(bytes(data, 'utf-8'),addr)):
+#			print("sending...")
+#			print(datagram)
 #		data = line
-		datagram = json.loads(datagram)
-		datagram["data"] = str(line)
+#		datagram = json.loads(datagram)
+#		datagram["data"] = str(line)
 
 s.close()
+
